83aa3b4c3c5e73bf2117f4d5b89d6426dc4626c4
showyourwork <showyourwork>
2022-09-07 08:38:26 +0200

    [showyourwork] overleaf sync

--- src/scripts/check_asassn-21js.py
+++ src/scripts/check_asassn-21js.py
@@ -15,12 +15,9 @@ t = ascii.read(paths.data / fin)
 
 t['MJD'] = t['HJD']-2400000.5
 
-
 # reject noisy points
 t = t[(t['flux(mJy)']<30)]
 print('rejected ASASSN points with high fluxes in both bands')
-
-
 
 fig, (ax) = plt.subplots(1,1,figsize=(12,6))
 ax.errorbar(t['MJD'],t['flux(mJy)'],yerr=t['flux_err'],fmt='.')
@@ -84,45 +81,113 @@ tV = tV[(tV['flux_err']<cum_reqV)]
 tg = tg[(tg['flux_err']<cum_reqg)]
 print('rejecting noisy points in g and V data separately in ASASSN')
 
-tmin, tmax = 58290, 58360
-
-(V_flux_norm, V_flux_norm_err)= \
-    mean_rms_region(tV['MJD'],tV['flux(mJy)'],tV['flux_err'], tmin, tmax)
-
-(g_flux_norm, g_flux_norm_err)= \
-    mean_rms_region(tg['MJD'],tg['flux(mJy)'],tg['flux_err'], tmin, tmax)
-
-print(V_flux_norm, g_flux_norm)
-
-tV['fnorm'] = tV['flux(mJy)']/V_flux_norm
-tg['fnorm'] = tg['flux(mJy)']/g_flux_norm
-
-tV['fnormerr'] = tV['flux_err']/V_flux_norm
-tg['fnormerr'] = tg['flux_err']/g_flux_norm
+# tmin, tmax = 58290, 58360
+#
+# (V_flux_norm, V_flux_norm_err)= \
+#     mean_rms_region(tV['MJD'],tV['flux(mJy)'],tV['flux_err'], tmin, tmax)
+#
+# (g_flux_norm, g_flux_norm_err)= \
+#     mean_rms_region(tg['MJD'],tg['flux(mJy)'],tg['flux_err'], tmin, tmax)
+#
+# print(V_flux_norm, g_flux_norm)
+#
+# tV['fnorm'] = tV['flux(mJy)']/V_flux_norm
+# tg['fnorm'] = tg['flux(mJy)']/g_flux_norm
+#
+# tV['fnormerr'] = tV['flux_err']/V_flux_norm
+# tg['fnormerr'] = tg['flux_err']/g_flux_norm
 
 fig, (ax) = plt.subplots(1,1,figsize=(12,6))
+
+# 1.973 2.200 at delta 0.227 over 202 days
+tedges = np.arange(57420.20-0.5,58336.02+0.5,1.0002237)
+
+(tc_binV, fc_binV, fsigc_binV, fc_npoiV) = \
+    rebinsig(tV['MJD'],tV['flux(mJy)'], tedges, ax=ax, err=tV['flux_err'])
+
+tedges = np.arange(58282.163-0.5,59822.02+0.5,1.0002237)
+
+(tc_bing, fc_bing, fsigc_bing, fc_npoig) = \
+    rebinsig(tg['MJD'],tg['flux(mJy)'], tedges, ax=ax, err=tg['flux_err'])
+
 ax.set_ylabel('Flux [mJy]')
 ax.set_xlabel('Epoch [MJD]')
 ax.set_title('data from {}'.format(fin))
 ax.errorbar(tV['MJD'],tV['flux(mJy)'],yerr=tV['flux_err'],label='V',fmt='.')
 ax.errorbar(tg['MJD'],tg['flux(mJy)'],yerr=tg['flux_err'],label='g',fmt='.')
+
+ax.errorbar(tc_binV,fc_binV,yerr=fsigc_binV,label='V2',fmt='.')
+
+ax.errorbar(tc_bing,fc_bing,yerr=fsigc_bing,label='g2',fmt='.')
+
 fig.savefig('_check_asassn3.pdf')
 
 fig, (ax) = plt.subplots(1,1,figsize=(12,6))
-ax.set_ylabel('Flux [normalised]')
+
+ax.set_ylabel('Flux [mJy]')
 ax.set_xlabel('Epoch [MJD]')
-ax.set_title('Normalised ASASSN data {}'.format(fin))
-#ax.errorbar(tV['MJD'],tV['fnorm'],yerr=tV['fnormerr'],label='V',fmt='.',alpha=0.1)
-#ax.errorbar(tg['MJD'],tg['fnorm'],yerr=tg['fnormerr'],label='g',fmt='.',alpha=0.1)
-ax.scatter(tV['MJD'],tV['fnorm'],label='V',
-    marker='o',edgecolors='none',alpha=0.1)
-ax.scatter(tg['MJD'],tg['fnorm'],label='g',
-    marker='o',edgecolors='none',alpha=0.1)
+ax.set_title('data from {}'.format(fin))
+
+ax.errorbar(tc_binV,fc_binV,yerr=fsigc_binV/np.sqrt(fc_npoiV),label='V2',fmt='.')
+ax.errorbar(tc_bing,fc_bing,yerr=fsigc_bing/np.sqrt(fc_npoig),label='g2',fmt='.')
 fig.savefig('_check_asassn4.pdf')
+
+#
+# fig, (ax) = plt.subplots(1,1,figsize=(12,6))
+# ax.set_ylabel('Flux [normalised]')
+# ax.set_xlabel('Epoch [MJD]')
+# ax.set_title('Normalised ASASSN data {}'.format(fin))
+#
+# ax.scatter(tV['MJD'],tV['fnorm'],label='V',
+#     marker='o',edgecolors='none',alpha=0.1)
+# ax.scatter(tg['MJD'],tg['fnorm'],label='g',
+#     marker='o',edgecolors='none',alpha=0.1)
+# fig.savefig('_check_asassn4.pdf')
+
+
+#ax.errorbar(tc_bing,fc_bing,yerr=fsigc_bing,label='g2',fmt='.')
+
+binned_g_err = fsigc_bing/np.sqrt(np.sqrt(fc_npoig))
+
+(cum_bing, xbing, cumbing) = hist_errors (binned_g_err,frac_keep=0.90)
+print(cum_bing)
+
+
+binned_V_err = fsigc_binV/np.sqrt(np.sqrt(fc_npoiV))
+
+(cum_binV, xbinV, cumbinV) = hist_errors (binned_V_err,frac_keep=0.90)
+print(cum_bing,cum_binV)
+
+
+fig, (ax1,ax2) = plt.subplots(1,2,figsize=(12,6))
+# cumulative plots....
+ax1.plot(xbing,cumbing)
+ax1.set_xlabel('g Flux err')
+ax1.set_ylabel('Cumulative fraction')
+ax2.plot(xbinV,cumbinV)
+ax2.set_xlabel('V Flux err')
+ax2.set_ylabel('Cumulative fraction')
+ax2.set_title('data from {}'.format(fin))
+
+# now filter binned g band flux on errors
+mg = (binned_g_err<cum_bing)
+mV = (binned_V_err<cum_binV)
+fig, (ax) = plt.subplots(1,1,figsize=(12,6))
+ax.set_ylabel('Flux [mJy]')
+ax.set_xlabel('Epoch [MJD]')
+ax.errorbar(tc_bing[mg],fc_bing[mg],yerr=binned_g_err[mg],label='g Band',fmt='.')
+ax.errorbar(tc_binV[mV],fc_binV[mV],yerr=binned_V_err[mV],label='V band',fmt='.')
+
+ax.set_title('ASASSN-21js light curve')
+ax.legend()
+fig.savefig('_check_asassn6.pdf')
+
+
 plt.show()
 
 
 tn = vstack([tV,tg])
 tn['Survey'] = "ASASSN"
 
-tn.write(paths.data / 'obs_ASASSN-21js_ASASSN.ecsv',format='ascii.ecsv',overwrite=True)
+tn.write(paths.data / 'obs_ASASSN-21js_ASASSN.ecsv',
+    format='ascii.ecsv',overwrite=True)
--- src/scripts/photomutils.py
+++ src/scripts/photomutils.py
@@ -21,3 +21,69 @@ def mean_rms_region (t,f,e, tmin, tmax):
     f_std = np.std(f[m])
 
     return f_mean, f_std
+
+
+
+def rebinsig(t, f, tedges, ax=None, err=None):
+    from astropy.stats import sigma_clip
+    import matplotlib.patches as patches
+
+    tc_bin = np.array([])
+    fc_bin = np.array([])
+    fsigc_bin = np.array([])
+    fc_npoi = np.array([])
+
+    t_lower = tedges[:-1]
+    t_upper = tedges[1:]
+    t_midpoint = (t_lower+t_upper)/2.
+
+    for t_mid, t_low, t_high in zip(t_midpoint, t_lower, t_upper):
+        # select points from flux that have
+        selpoints = ((t > t_low) * (t < t_high))
+        fluxsel = f[selpoints]
+        timesel = t[selpoints]
+
+        # if there's just one point, just copy the values over
+        if (fluxsel.size == 1):
+        #    print('time {} has {} points'.format(t_mid, fluxsel.size))
+            tc_bin = np.append(tc_bin, timesel)
+            fc_bin = np.append(fc_bin, fluxsel)
+            if err is not None:
+                fsigc_bin = np.append(fsigc_bin, err[selpoints].value)
+            else:
+                fsigc_bin = np.append(fsigc_bin, 0.)
+
+            fc_npoi = np.append(fc_npoi, 1.)
+
+       # if there are two points, don't run any sigma clipping
+        if (fluxsel.size == 2):
+        #    print('time {} has {} points'.format(t_mid, fluxsel.size))
+            tc_bin = np.append(tc_bin, np.mean(timesel))
+            fc_bin = np.append(fc_bin, np.mean(fluxsel))
+            if err is not None:
+                fsigc_bin = np.append(fsigc_bin, np.sqrt(np.sum(np.power(err[selpoints],2))))
+            else:
+                fsigc_bin = np.append(fsigc_bin, np.ma.std(fluxsel))
+
+            fc_npoi = np.append(fc_npoi, 2.)
+
+        # more than two points, go sigma clipping
+        if (fluxsel.size > 2):
+        #    print('time {} has {} points'.format(t_mid, fluxsel.size))
+
+            meanflux = sigma_clip(fluxsel, sigma=3, maxiters=1, masked=True)
+            meane = np.ma.mean(meanflux)
+            # NOTE!!! should select the unmasked times and take the average of that, maybe???
+            if np.isfinite(meane):
+                tc_bin = np.append(tc_bin, np.ma.mean(timesel))
+                fc_bin = np.append(fc_bin, np.ma.mean(meanflux))
+                fsigc_bin = np.append(fsigc_bin, np.ma.std(meanflux))
+                fc_npoi = np.append(fc_npoi, np.sum(selpoints))
+
+        if (ax is not None) and (fluxsel.size > 0):
+            f_low = np.min(fluxsel)
+            f_hig = np.max(fluxsel)
+            rect = patches.Rectangle((t_low,f_low), t_high-t_low, f_hig-f_low, edgecolor='r',facecolor='none')
+            ax.add_patch(rect)
+
+    return (tc_bin, fc_bin, fsigc_bin, fc_npoi)
